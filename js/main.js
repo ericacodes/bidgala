efda2191e2df3002b23bc96ba46a50843cd50a88
Erica Sun <ericasun@seas.upenn.edu>
2021-05-19 16:55:36 -0400

    functionality done

--- js/main.js
+++ js/main.js
@@ -12,10 +12,13 @@ $(document).ready(() => {
 function getSearchResults(searchInput) {
   axios.get(`http://www.omdbapi.com/?apikey=d84c5776&s=${searchInput}`)
   .then(response => {
-    console.log(response)
     let searchResults = response.data.Search;
     let output = '';
+    let watchlistId = JSON.parse(localStorage.getItem("movieIdArray"));
     $.each(searchResults, (index, result) => {
+      let name = result.Title.replace("'", "\\'");
+      const disabledOrNot = watchlistId.includes(result.imdbID) ? 'disabled' : '';
+
       output += `
       <container class="row result">
         <div class="col">
@@ -28,10 +31,11 @@ function getSearchResults(searchInput) {
           <p>${result.Year}</p>
         </div>
         <div class="col">
-          <a onclick="addToWatchlist('${result.imdbID}')" class="btn btn-primary">Add</a>
+          <button onclick="addToWatchlist('${result.imdbID}', '${result.Poster}', '${name}', '${result.Year}')" class="btn btn-primary" ${disabledOrNot}>Add</button>
         </div>
       </container>
       `
+      
     });
 
     console.log(output);
@@ -41,64 +45,58 @@ function getSearchResults(searchInput) {
 }
 
 function getWatchlist() {
-  let parsedArray = JSON.parse(localStorage.getItem("idArray"));
-  console.log(parsedArray);
+  let parsedInfo = JSON.parse(localStorage.getItem("movieInfoArray"));
 
-  if (parsedArray) {
+  if (parsedInfo) {
     let output = '';
-    $.each(parsedArray, (index, id) => {
-      console.log(typeof id)
-      axios.get(`http://www.omdbapi.com/?&apikey=d84c5776&i=${id}`)
-      .then(response => {
-        console.log(response);
-      })
-      .catch(error => {
-        console.log(error);
-      })
+    $.each(parsedInfo, (index, movie) => {
+      output += `
+      <container class="row result">
+        <div class="col">
+          <img
+            src="${movie.poster}"
+          />
+        </div>
+        <div class="col">
+          <h6>${movie.title}</h6>
+          <p>${movie.year}</p>
+        </div>
+        <div class="col">
+          <button onclick="removeFromWatchlist(${index})" class="btn btn-primary">Remove</button>
+        </div>
+      </container>
+    `
     });
+    $('#list').html(output);
   }
-
-  // $.each(parsedArray, (index, result) => {
-  //   output += `
-  //   <container class="row result">
-  //     <div class="col">
-  //       <img
-  //         src="${result.Poster}"
-  //       />
-  //     </div>
-  //     <div class="col">
-  //       <h6>${result.Title}</h6>
-  //       <p>${result.Year}</p>
-  //     </div>
-  //     <div class="col">
-  //       <a onclick="addToWatchlist(${result.imdbID})" class="btn btn-primary">Add</a>
-  //     </div>
-  //   </container>
-  //   `
-  // });
-  // console.log(output);
-  // $('#list').html(output);
-
 }
 
-function addToWatchlist(id) {
-  console.log(typeof id);
-
-  if (localStorage.getItem("idArray") == null) {
-    localStorage.setItem("idArray", "[]");
+function addToWatchlist(id, poster, title, year) {
+  let movieInfo = {
+    poster, title, year
   }
 
-  let parsedArray = JSON.parse(localStorage.getItem("idArray"));
-  parsedArray.push(id);
-  localStorage.setItem("idArray", JSON.stringify(parsedArray));
+  if (localStorage.getItem("movieInfoArray") == null) {
+    localStorage.setItem("movieInfoArray", "[]");
+    localStorage.setItem("movieIdArray", "[]");
+  }
+  let parsedInfo = JSON.parse(localStorage.getItem("movieInfoArray"));
+  let parsedId = JSON.parse(localStorage.getItem("movieIdArray"));
+  parsedInfo.push(movieInfo);
+  parsedId.push(id);
+  localStorage.setItem("movieInfoArray", JSON.stringify(parsedInfo));
+  localStorage.setItem("movieIdArray", JSON.stringify(parsedId));
 
   getWatchlist();
 }
 
+function removeFromWatchlist(index) {
+  let parsedInfo = JSON.parse(localStorage.getItem("movieInfoArray"));
+  let parsedId = JSON.parse(localStorage.getItem("movieIdArray"));
+  parsedInfo.splice(index, 1);
+  parsedId.splice(index, 1);
+  localStorage.setItem("movieInfoArray", JSON.stringify(parsedInfo));
+  localStorage.setItem("movieIdArray", JSON.stringify(parsedId));
 
-
-
-// addNewEntry
-// readData 
-// saveData 
-// updateEntries
+  getWatchlist();
+}
